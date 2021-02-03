f39613b25a5935f7f3e7a6716e80b37bd62fd9a9
tong.luo <luotong@dazhangfang.com>
2021-02-03 10:56:55 +0800

    add define code

---
+++ part-1/define-test1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+#define LENGTH 10
+#define WIDTH 5
+#define NEWLINE '\n'
+
+int main()
+{
+int area;
+area = LENGTH * WIDTH;
+cout << area << NEWLINE << endl;
+return 0;
+}
