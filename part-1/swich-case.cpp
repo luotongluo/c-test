b4c1d0ee6c1c13e37e9dfdcda38accc5c7b3e316
tong.luo <luotong@dazhangfang.com>
2021-02-03 12:34:51 +0800

    commit

---
+++ part-1/swich-case.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int a =10;
+    for (int b = 0; a < b;a ++){
+    swich(b){
+        case 1 :
+        cout << "value is "<< b <<endl;
+        break;
+        case 2 :
+        cout << "value is "<< b <<endl;
+        break;
+        default:
+        cout << "this is defult code" << endl;
+        break;
+
+    }
+    }
+
+}
