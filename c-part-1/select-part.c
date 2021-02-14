85bf6209c0580ad3199e4852782680fbb300dc41
tong.luo <luo12828@foxmail.com>
2021-02-14 15:59:34 +0800

    add select part

---
+++ c-part-1/select-part.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void main()
+{
+int x,y,z,system;
+printf("请输入三个整数");
+scanf("%d%d%d",&x,&y,&z);
+if(x < y){
+system = x;
+x = y;
+y = system;
+
+}
+if(x < z){
+system = x;
+x = z;
+z = system;
+}
+if (y < z){
+system = z;
+z = y;
+y = system;
+}
+printf("x = %d,y = %d,z = %d",x,y,z);
+}
