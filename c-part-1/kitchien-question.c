801de83adc64624fe67a21228e9b56bb8439b55f
tong.luo <luo12828@foxmail.com>
2021-02-15 00:05:05 +0800

    add

--- c-part-1/kitchien-question.c
+++ c-part-1/kitchien-question.c
@@ -2,6 +2,8 @@
 /**
 鸡翁1，钱值5；鸡母1钱值3，鸡雏3 值钱1
 百钱买百鸡，问 鸡翁，鸡雏，鸡母各几只
+c语言中的向上取整
+https://zhidao.baidu.com/question/18167140.html
 */
 void main()
 {
@@ -11,13 +13,13 @@ int x,y,z;
 int mon = 100;
 int xmon = 5;
 int ymon = 3;
-int z3mon = 1 / 3;
+double z3mon = 1 / 3;
 //鸡翁的数量不会超过20只
 int loopX = 100 / xmon;
 printf("鸡翁的 loopx ： %d \n",loopX);
 int loopY = 100 / ymon;
 printf("鸡母的 loopY ： %d \n",loopY);
-int loopZ = (100 / z3mon) > 100 ? 100 : (100 / z3mon);
+int loopZ = (floor(100 / z3mon)) > 100 ? 100 : (100 / z3mon);
 printf("鸡母的 loopY ： %d \n",loopZ);
 for (x = 1;x <= loopX; x ++ ){
 //鸡母最多能购买 100 / 3 向上取整
