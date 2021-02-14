d11df6278e0b691235c0bc9774b37ed547eefa98
tong.luo <luo12828@foxmail.com>
2021-02-15 00:24:01 +0800

    add

--- c-part-1/calcute100.c
+++ c-part-1/calcute100.c
@@ -7,7 +7,9 @@ void main()
     int ymon = 3;
     int z3mon = 1.0 / 3.0;
     printf("请输入 鸡翁，鸡母，鸡雏的数量\n");
-    scanf("%d,%d,%d",&x,&y,&z);
+    scanf("%d",&x);
+    scanf("%d",&y);
+    scanf("%d",&z);
     int value =xmon * x + ymon * y + z3mon * z;
     printf(" %d:",value);
 }
