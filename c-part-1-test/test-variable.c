ab3bc0b4453b8e0bb3346ff0f11af5522445528c
tong.luo <luo12828@foxmail.com>
2021-02-17 15:56:39 +0800

    add print a of num

--- c-part-1-test/test-variable.c
+++ c-part-1-test/test-variable.c
@@ -3,15 +3,16 @@
 
 */
 void auto_ststic(){
-int car_auto = 0;
-static var_auto = 0;
-printf("car_auto = %d,var_auto = %d \n",car_auto,var_auto);
-++car_auto;
-++var_auto;
+    int car_auto = 0;
+    static var_auto = 0;
+    printf("car_auto = %d,var_auto = %d \n",car_auto,var_auto);
+    ++car_auto;
+    ++var_auto;
 }
 
 void main(){
-    for(int a =0;a<2 ;a++){
+    int a;
+    for( a =0;a<2 ;a++){
         auto_ststic();
     }
 }
