bec594d55afcea1ac39770356cabb155ae5913ff
tong.luo <tong.luo@example.com>
2021-02-20 10:09:02 +0800

    ad

--- part-4/Template-test.cpp
+++ part-4/Template-test.cpp
@@ -1,20 +1,18 @@
 #include<iostream>
 using namespace std;
 template <typename T>
-T max(T m1,T m2)
-{
-    return (m1 > m2) ? m1 : m2;
-}
-
-template <typename T>
-T min(T m1,T m2)
-{
-    return (m1 > m2 )? m2 : m1;
-}
-int main()
-{
-    cout << max("abc","aBc") << endl;
-    cout << min("abc","aBc") << endl;
-    cout << min<int>(1.2,3.4) << endl;
-    return 0;
-}
+T max(T m1 , T m2)
+	{
+	return (m1>m2)?m1:m2;
+	}
+template<typename T>
+T min(T m1 , T m2)
+	{
+	return(m1<m2)? m1:m2;
+	}
+void main()
+	{
+	cout<<max("abc" , "aBC")<<endl;
+	cout<<min(2.3 , 5.8)<<endl;
+	cout<<min<int>(8 , 3)<<endl;
+	}
