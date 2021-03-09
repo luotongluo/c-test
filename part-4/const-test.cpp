c0659b6e634f39e341e30b5ca240c8a0f2c25596
tong.luo <luotong@dazhangfang.com>
2021-03-09 15:41:59 +0800

    add

---
+++ part-4/const-test.cpp
@@ -0,0 +1,29 @@
+#include <iosteam>
+using namespace std;
+
+class Base{
+int x,y;
+public:
+    Base(){
+    cout << "Base 无参构造函数" << endl;
+    }
+
+    ~Base(){
+    cout << "Base 析构函数" << endl;
+    }
+};
+
+class BaseB:public Base{
+BaseB(){
+    cout << "Base 无参构造函数" << endl;
+    }
+
+    ~BaseB(){
+    cout << "Base 析构函数" << endl;
+    }
+};
+
+int main(){
+ BaseB b;
+ return 0;
+}
